6ed324b25c3362057ef36a0c8737d3f0f004006a
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-09-17 18:52:35 -0300

    modificaciónes menores de estructura jpa

--- Día 04 - Acceso a datos y ORMs/learning/TennisAppPeiReactJava/tennis-api/src/main/java/com/baufest/tennis/springtennis/model/Jugador.java
+++ Día 04 - Acceso a datos y ORMs/learning/TennisAppPeiReactJava/tennis-api/src/main/java/com/baufest/tennis/springtennis/model/Jugador.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "Jugador")
+@Table(name = "Jugador_test")
 public class Jugador{
 
 	@Id
--- Día 04 - Acceso a datos y ORMs/learning/TennisAppPeiReactJava/tennis-api/src/main/java/com/baufest/tennis/springtennis/model/Partido.java
+++ Día 04 - Acceso a datos y ORMs/learning/TennisAppPeiReactJava/tennis-api/src/main/java/com/baufest/tennis/springtennis/model/Partido.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "partido_test")
 public class Partido {
 
 	@Id
