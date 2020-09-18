e50870d9107e20e1b826e5dc9efc3d92958c06d0
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-09-18 14:07:52 -0300

    inicio app react y algunos componentes

---
+++ Día 05 - React Sesión I (introducción)/learning/react-app/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <p>Hello World from a component</p>
+        )
+    }
+}
+
+export default App;
---
+++ Día 05 - React Sesión I (introducción)/learning/react-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const greeting = <p>Hello World from React!</p>;
+
+function myFunction() {
+    return <p>Functional component</p>;
+}
+
+const myFunctionVariable = () => {
+    return <p>Functional component variable</p>;
+};
+
+const myArrowFunction = () => <p>Arrow func</p>;
+
+ReactDOM.render(<App/>, document.getElementById('app'));
